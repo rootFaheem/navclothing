e7f7008a120285bd5e05b8aa0ebb969e0801fe8b
Faheem <faheemtech1@gmail.com>
2019-01-31 15:44:58 +0530

    jasonwebtoken added and Bearer + tocken format applied

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://faheem:jmi123@ds147079.mlab.com:47079/navclothing"
+  mongoURI: "mongodb://faheem:jmi123@ds147079.mlab.com:47079/navclothing",
+  secretOrKey: "secret"
 };
