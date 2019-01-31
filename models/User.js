c6a1bbe39e125d86c86f3b09bb0fb13c44219e31
Faheem <faheemtech1@gmail.com>
2019-01-31 14:06:37 +0530

    models for user added

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    Default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
